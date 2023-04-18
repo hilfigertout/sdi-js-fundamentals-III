8f6d50a31d003267d31e55f3f8689eea0223497e
Ian Roberts <ijr2399@gmail.com>
2023-04-18 10:56:29 -0500

    TODO list working, not styled

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('My code is running');
+let inputField = document.querySelector("#input1");
+let text = "";
+inputField.addEventListener("input", (eventObject) => {
+    text = eventObject.target.value;
+});
+let btn = document.querySelector("#button1");
+btn.addEventListener("click", (eventObject) => {
+    eventObject.preventDefault();
+    let newItem = document.createElement("li");
+    newItem.addEventListener('click', (eventObject) => {
+        eventObject.target.remove();
+    });
+    newItem.innerHTML = text;
+    document.querySelector('ul').appendChild(newItem);
+    inputField.value = "";
+})
+
